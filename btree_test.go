2ba45a374a650048f8f30be23d38d64e3c2bf315
MoltenWolfCub <oliver@tomlins.org.uk>
2024-05-25 21:12:45 +0100

    wrote some proper tests

--- btree_test.go
+++ btree_test.go
@@ -1,19 +1,85 @@
 package btree
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestInsert(t *testing.T) {
-	tree := NewBTree()
-	tree.rootNode.addItem(5)
-	fmt.Println(tree.rootNode.keys)
-	tree.rootNode.addItem(7)
-	tree.rootNode.addItem(2)
-	fmt.Println(tree.rootNode.keys)
-	tree.rootNode.addItem(3)
-	fmt.Println(tree.rootNode.keys)
-	tree.rootNode.addItem(6)
-	fmt.Println(tree.rootNode.keys)
+func slicesDifferent[T comparable](a []T, b []T) bool {
+	if len(a) != len(b) {
+		return true
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func TestOneItem(t *testing.T) {
+	node := Node{}
+
+	node.addItem(7)
+
+	if slicesDifferent(node.keys, []NodeInt{7}) {
+		t.Errorf("Node.addItem(7); provides %v; want [7]", node.keys)
+	}
+}
+
+func TestTwoItemsRightOrder(t *testing.T) {
+	node := Node{}
+
+	node.addItem(2)
+	node.addItem(3)
+
+	if slicesDifferent(node.keys, []NodeInt{2, 3}) {
+		t.Errorf("Added 2 items correctly ordered. Got %v; wanted [2, 3]", node.keys)
+	}
+}
+
+func TestTwoItemsWrongOrder(t *testing.T) {
+	node := Node{}
+
+	node.addItem(6)
+	node.addItem(4)
+
+	if slicesDifferent(node.keys, []NodeInt{4, 6}) {
+		t.Errorf("Added 2 items incorrectly ordered. Got %v; wanted [4, 6]", node.keys)
+	}
+}
+
+func TestAddBetweenTwoItems(t *testing.T) {
+	node := Node{}
+	node.addItem(15)
+	node.addItem(18)
+
+	node.addItem(17)
+
+	if slicesDifferent(node.keys, []NodeInt{15, 17, 18}) {
+		t.Errorf("Added item between 2 other items. Got %v; wanted [15, 17, 18]", node.keys)
+	}
+}
+
+func TestAddBeforeTwoItems(t *testing.T) {
+	node := Node{}
+	node.addItem(15)
+	node.addItem(18)
+
+	node.addItem(13)
+
+	if slicesDifferent(node.keys, []NodeInt{13, 15, 18}) {
+		t.Errorf("Added item before 2 other items. Got %v; wanted [13, 15, 18]", node.keys)
+	}
+}
+
+func TestAddAfterTwoItems(t *testing.T) {
+	node := Node{}
+	node.addItem(15)
+	node.addItem(18)
+
+	node.addItem(22)
+
+	if slicesDifferent(node.keys, []NodeInt{15, 18, 22}) {
+		t.Errorf("Added item after 2 other items. Got %v; wanted [15, 18, 22]", node.keys)
+	}
 }
